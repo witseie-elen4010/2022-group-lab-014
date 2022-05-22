a849f3b162bb41ed73118fb6c29635dc63e4afc9
Mikeygoldman1 <goldmanmg@gmail.com>
2022-05-22 16:25:08 +0200

    Added in keyboard and functionality into multiplayer page

--- src/public/scripts/multi-player.js
+++ src/public/scripts/multi-player.js
@@ -4,16 +4,73 @@
     const numCells=row*col;
     for (i = 0; i < numCells; i++) {
       let cell = document.createElement("div");
+      cell.setAttribute('id', 'cell' + i)
       container.appendChild(cell).className = "gameGrid-item";
     };
   };
-  
+  function makeKeyboard(KeyRow) {
+    const keyboard = document.getElementById("keyboard");
+    var newDiv = document.createElement("div");
+    newDiv.className = "row align-items-center";
+    if (KeyRow[0]==="Q") {
+      newDiv.style = "margin-top:200px"
+    } 
+     for (var i = 0; i < KeyRow.length; i++) {
+       var key = document.createElement('button');
+       key.setAttribute('id', KeyRow[i])
+       key.type = 'button';
+       key.className = "col-sm btn btn-light btn btn-outline-dark";
+       key.innerHTML = KeyRow[i];
+       newDiv.appendChild(key);
+       keyboard.appendChild(newDiv)
+     }
+   };
+   
+   function KeysInGrid(KeyRow, cellCount) {
+     let count = 0
+     let currentRow = 0
+     while (count != 3) {
+       for (var j = 0; j < KeyRow[count].length; j++) {
+         const inKey = document.getElementById(KeyRow[count][j]);
+         inKey.addEventListener('click', function () {
+           if ((inKey.innerHTML !== "DELETE")) {
+             if(Math.floor(cellCount/5) === currentRow && inKey.innerHTML !== "ENTER"){
+               const cell = document.getElementById('cell' + (cellCount))
+               cell.innerHTML = inKey.innerHTML
+               cellCount = cellCount + 1;
+             }
+             else if(inKey.innerHTML === "ENTER" && cellCount % 5 === 0 ){
+               if (Math.floor(cellCount/5) !== currentRow) {
+                 currentRow+=1
+               }
+             }
+           }
+           else if (inKey.innerHTML == "DELETE" && cellCount > 0) {
+             cellCount = cellCount - 1;
+             document.getElementById('cell' + cellCount).innerHTML = ""
+           }
+         }, false)
+       }
+       count = count + 1
+     }
+   };
+   
+
+  var firstRowKeys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+  var secondRowKeys = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  var thirdRowKeys = ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE"];
+  let RowOfKeys = [firstRowKeys, secondRowKeys, thirdRowKeys];
   const button1 = document.getElementById('MultiPlayerGame')
   
   button1.addEventListener('click', function () {
   button1.style.display = 'none';
   const container = document.getElementById("container");
+  let cellCount = 0
    makeRows(6, 5);
+   makeKeyboard(firstRowKeys)
+   makeKeyboard(secondRowKeys)
+   makeKeyboard(thirdRowKeys)
+   KeysInGrid(RowOfKeys, cellCount)
   }, false)
   
   
