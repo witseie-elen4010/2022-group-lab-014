ded9c59b9ada674cf33a44c73e8f0c18119fad11
jasonmervis <jasonmervis1@gmail.com>
2022-06-03 11:34:51 +0200

    Added database testing which does not test the database, but mocks how the method of the database works

--- src/mockRoutes.js
+++ src/mockRoutes.js
@@ -3,6 +3,11 @@ const express = require('express')
 const mainRouter = express.Router()
 const dict = require('./dictionary.js')
 
+const userdetails = {
+  username: 'NO LOG IN',
+  games_played: 0,
+  games_won: 0
+}
 
 mainRouter.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'index.html'))
@@ -32,5 +37,8 @@ mainRouter.get('/api/isValid', function (req, res) {
   res.send(JSON.stringify(dict.getJSON()))
 })
 
+mainRouter.get('/api/user', function (req, res) {
+  res.send(JSON.stringify(userdetails))
+})
 
 module.exports = mainRouter
