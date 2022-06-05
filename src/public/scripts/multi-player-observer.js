c5d97c5691fc9bb8c3f9392584bfabc43d81672a
Aiden Smith <smithaideny@gmail.com>
2022-06-05 18:28:27 +0200

    Adding names for the user set word grids

--- src/public/scripts/multi-player-observer.js
+++ src/public/scripts/multi-player-observer.js
@@ -1,28 +1,26 @@
-  const container2 = document.getElementById("container2");  
-  const container1 = document.getElementById("container1");  
-  const container3 = document.getElementById("container3");  
-  let num_of_players=3;
-  function makeRows(row, col) {
-    container1.style.setProperty('--grid-row', row);
-    container1.style.setProperty('--grid-col', col);
-    const numCells=row*col;
-    for (i = 0; i < numCells; i++) {
-      let cell = document.createElement("div");
-      cell.setAttribute('id', 'opp1Cell' + i)
-      container1.appendChild(cell).className = 'opp1Grid-item';
-    };
+const container2 = document.getElementById('container2')
+const container1 = document.getElementById('container1')
+const container3 = document.getElementById('container3')
+const num_of_players = 3
+function makeRows (row, col) {
+  container1.style.setProperty('--grid-row', row)
+  container1.style.setProperty('--grid-col', col)
+  const numCells = row * col
+  for (i = 0; i < numCells; i++) {
+    const cell = document.createElement('div')
+    cell.setAttribute('id', 'opp1Cell' + i)
+    container1.appendChild(cell).className = 'opp1Grid-item'
   };
 };
 
-  function makeRows2(row, col) {
-    container2.style.setProperty('--grid-row', row);
-    container2.style.setProperty('--grid-col', col);
-    const numCells=row*col;
-    for (i = 0; i < numCells; i++) {
-      let cell = document.createElement("div");
-      cell.setAttribute('id', 'opp2Cell' + i)
-      container2.appendChild(cell).className = 'opp2Grid-item';
-    };
+function makeRows2 (row, col) {
+  container2.style.setProperty('--grid-row', row)
+  container2.style.setProperty('--grid-col', col)
+  const numCells = row * col
+  for (i = 0; i < numCells; i++) {
+    const cell = document.createElement('div')
+    cell.setAttribute('id', 'opp2Cell' + i)
+    container2.appendChild(cell).className = 'opp2Grid-item'
   };
 };
 
@@ -59,41 +57,43 @@ if (num_of_players == 3) {
         count += 1
       })
     })
+  hOpponent1.style.display = 'initial'
+  hOpponent2.style.display = 'initial'
 }
 
-const refresh=document.getElementById('refresh')
+const refresh = document.getElementById('refresh')
 
 refresh.addEventListener('click', function () {
-  changeColour1() 
-  changeColour2() 
-},false)
+  changeColour1()
+  changeColour2()
+}, false)
 
-function changeColour1() {
+function changeColour1 () {
   fetch('/api/sendColour2')
     .then(function (response) {
       if (response.ok) { return response.json() } else { throw 'Failed to fetch' }
     })
     .then(function (data) {
-      for (let i = 0; i<30; i++) {
+      for (let i = 0; i < 30; i++) {
         const cell = document.getElementById('opp1Cell' + i)
-        cell.className = data[i]   
+        cell.className = data[i]
       }
     })
     .catch(function (e) {
       console.log(e)
     })
-  }
-function changeColour2() {
+}
+function changeColour2 () {
   fetch('/api/sendColour1')
     .then(function (response) {
       if (response.ok) { return response.json() } else { throw 'Failed to fetch' }
     })
     .then(function (data) {
-      for (let i = 0; i<30; i++) {
-        let cell = document.getElementById('opp2Cell' + i)
+      for (let i = 0; i < 30; i++) {
+        const cell = document.getElementById('opp2Cell' + i)
         let newClass = data[i]
         newClass = newClass.replace('opp2Grid-item', 'opp1Grid-item')
-        cell.className = newClass      
+        cell.className = newClass
       }
     })
     .catch(function (e) {
